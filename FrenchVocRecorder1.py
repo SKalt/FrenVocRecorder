bd54fde432236a048e8c2e34a7e7b7e175b0f63b
SKalt <kalt.steven@gmail.com>
2015-01-07 23:50:23 +0100

    Updated the reader to iterate line-by-line, requiring you to select the neccessary items for the dictionary. Added another dictionary for phrases.

--- FrenchVocRecorder1.py
+++ FrenchVocRecorder1.py
@@ -1,13 +1,19 @@
+#!/usr/bin/env python
 import urllib3
 from bs4 import BeautifulSoup
 import csv
 
 http = urllib3.PoolManager()
 DictFile = open("FrenDict.csv","a")
+PhraseFile = open("PhrasesCSV.csv","a")
 DictWriter = csv.writer(DictFile)
+PhraseWriter = csv.writer(PhraseFile)
 StartText = """
 To quit, press enter 'q'
 To change language, enter 'change'
+Press enter to read the next line of the definition.
+To add a definition to your dictionary, press y and
+then enter.
 
 Look up:
 """
@@ -69,15 +75,35 @@ while True:
             else:
                 article = soup.find('div', {"class" : "article_bilingue"})
                 try:
-                    Entry1 = article.h1.text
-                    Translat_Set = article.find_all('div')
-
+                    Entry1 = article.h1.text.split()[1:]
+                    Entry1 = ' '.join(Entry1)
                     print(Entry1)
-                    for item in Translat_Set:
-                        defin = item.text
-                        print(defin)
-                        DictWriter.writerow(['fr', Entry1, defin])
+                    Translat_Set = article.find_all('div')
+                    try:
+                        Temp = 'y'
+                        TempList = []
+                        for item in Translat_Set:
+                            defin = item.text.split('\n')
+                            for subitem in defin:
+                                if len(subitem) > 0:
+                                    if Temp == 'y':
+                                        print(subitem)
+                                        TempList.append(subitem)
+                                        Temp = input()
+                                    else:
+                                        Temp = 'y'
+                    except:
+                        print('early')
                         
+                    try:
+                                
+                        for Thing in TempList:
+                            if '\xa0' in Thing:
+                                PhraseWriter.writerow(Thing.split('\xa0'))
+                            else:
+                                DictWriter.writerow(['fr', Entry1, Thing])
+                    except:
+                        print('later')
                 except:
                     print('you done fucked up mate-- see the try block')
                 #ZoneEntree = soup.find('div', {"class" : "ZoneEntree"}).text
